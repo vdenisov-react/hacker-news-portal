655af281d2c4a301f6d3c21b1421ffaa90bf9e34
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-05-15 16:21:23 +0300

    version 1.0.0

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import News from './containers/news/news';
+
+class App extends Component {
+  render() {
+    return (
+      <div class="container mt-5">
+        <News />
+      </div>
+    )
+  }
+}
+
+
+export default App;
